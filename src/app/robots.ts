0469e1bdacb93e17489bc96e236b49298ca4ef83
Taherankhan <pathantaheran1122@gmail.com>
2026-07-27 10:01:24 +0530

    Update robots.ts rules array syntax for search console crawlers

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,13 @@ export default function robots(): MetadataRoute.Robots {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://jsonmint.onrender.com';
   
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/private/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/'],
+        disallow: ['/api/'],
+      },
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
